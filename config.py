4db4727f2a6b1ec019eff8a25e6a4ac79b28cd05
bodakuntaajay <137863096+bodakuntaajay@users.noreply.github.com>
2025-10-19 17:17:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ import os
 class Config:
     """Base configuration settings for the Flask app."""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-very-secret-and-hard-to-guess-key'
-    SQLALCHEMY_DATABASE_URI = database_url
+    SQLALCHEMY_DATABASE_URI ='postgresql://ajaydb_mooe_user:2uCzgTOxUey4IJH1YrvFOxkq6fetkPBY@dpg-d3qc8i2li9vc73c7mg70-a/ajaydb_mooe'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Credentials for the single admin user
     ADMIN_USERNAME = 'admin'
     ADMIN_PASSWORD = 'admin123'
+
 
 
